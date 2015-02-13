fbdada7a9f9d3df1a603d64c8ff060057cee0b6a
suprita shankar <suprita.shankar@gmail.com>
2015-02-12 22:01:50 -0800

    add some content to how we approach the problem

---
+++ app/hip.py
@@ -0,0 +1,25 @@
+"""
+We use a [9][9] matrix to indicate the board of the Hip game.
+0 - it is empty
+1 - Blue
+2 - Red
+
+There are 2 approaches to this
+1. Is either all the given boxes are predefined and stored.
+No matter which inclination and position.
+2. Else we have function which takes the current input of the board and
+predicts whether a square is possible or not
+
+At any point of time as soon as a square is formed by either R or B, that person loses and the game terminates.
+"""
+
+"""
+First attempt to solve the problem 
+All the input will be given by the human itself.
+There will no human vs computer. 
+"""
+
+"""
+In this program we are taking input states of different board configuration.
+"""
+
